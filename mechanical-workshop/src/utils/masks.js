9e3b3c83793144369a6cd4c1d2abd24c70f8651b
ArthurLDS <arthurlds73@gmail.com>
2019-06-28 22:17:00 -0300

    test

--- mechanical-workshop/src/utils/masks.js
+++ mechanical-workshop/src/utils/masks.js
@@ -1,6 +1,6 @@
 class MasksService {
 
-    static formatPhone(text){
+    static formatPhone  (text){
         var x = text.replace(/\D+/g, '').match(/(\d{0,2})(\d{0,5})(\d{0,4})/);
         return !x[2] ? x[1] : '(' + x[1] + ') ' + x[2] + (x[3] ? '-' + x[3] : '');
     }
